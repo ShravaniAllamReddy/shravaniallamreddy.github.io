0745404e704282fbb0690910cba6102ceab5b909
shravaniallamreddy <shravani.allam@gmail.com>
2021-03-02 12:08:22 -0700

    modified routes path

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const routes = require("./controllers");
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
@@ -15,10 +16,11 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
+
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcontacts");
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
