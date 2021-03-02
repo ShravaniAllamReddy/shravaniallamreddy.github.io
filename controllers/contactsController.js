4877d9e67a0b5ec838badef04f80055b75cf04c5
shravaniallamreddy <shravani.allam@gmail.com>
2021-03-02 12:07:15 -0700

    added api route

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,26 +1,27 @@
 const db = require("../models");
-
+const router = require('express').Router();
 // Defining methods for the contactsController
-module.exports = {
-  findAll: function(req, res) {
+
+router.get('/', function(req, res) {
     db.Contact
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
-  },
-  findById: function(req, res) {
+  });
+
+  router.get('/:id', function(req, res) {
     db.Contact
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
-  },
-  create: function(req, res) {
+  });
+
+ router.post('/' , function(req, res) {
     db.Contact
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
-  }
+  });
  
- 
-};
+  module.exports = router;
